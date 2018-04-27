26f8e6ca41f85066410a551c1b72b1ffd03f01bd
esefberfu <33575745+esefberfu@users.noreply.github.com>
2018-04-27 11:02:30 +0300

    Update node_websocket_server.js

--- node_websocket_server.js
+++ node_websocket_server.js
@@ -1,138 +1,76 @@
-	var WebSocketServer = require('websocket').server;
-	var http = require('http');
+const WebSocket = require('ws');
 
-	var listDevice = {}; // drone listesi
-	var listClient = {}; // masaüstü veya diğer bilgisayarların tutuldu liste
-	var id_counter = 0;  // id bilgisi tutuluyor şimdilik geçici id'ler atanıyor. Daha sonrasında 
+var listDevice = {}; // drone listesi
+var listClient = {}; // masaüstü veya diğer bilgisayarların tutuldu liste
+var id_counter = 0;  // id bilgisi tutuluyor şimdilik geçici id'ler atanıyor. Daha sonrasında
 
-	var server = http.createServer(function(request, response) {
-		console.log((new Date()) + ' Received request for ' + request.url);
-		response.writeHead(404);
-		response.end();
-	});
-	server.listen(8080, function() {
-		console.log((new Date()) + ' Server is listening on port 8080');
-	});
+const wss = new WebSocket.Server({ port: 8080 });
 
-	wsServer = new WebSocketServer({
-		httpServer: server,
-		autoAcceptConnections: false
-	});
+wss.on('connection', function connection(ws, req) {
+    if(req.url === '/device')
+    {
+        device(ws);
+    }
+    else if(req.url === '/client')
+    {
+        client(ws);
+    }
+    else if(req.url === '/data')
+    {
+        data(ws);
+    }
+    else
+    {
+        ws.close();
+    }
 
-	function originIsAllowed(origin) {
-		// put logic here to detect whether the specified origin is allowed.
-		return true;
-	}
+});
 
-	// gelen bağlantılar işleniyor
-	wsServer.on('request', function(request) {
-		if (!originIsAllowed(request.origin)) {
-			request.reject();
-			console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-			return;
-		}
-		var connection = request.accept('echo-protocol', request.origin);
-		var reqUrl = request.resourceURL;
+function device(ws) {
+    //bir drone bağlandığında bilgisi tüm client'lere gönderilecek.
+    listDevice[id_counter] = ws;
+    console.log((new Date()) + ' Connection accepted. DEVICE-ID: ' + id_counter);
 
-		//device --> drone
-		//client --> pc, desktop
-		//data --> drone'daki veriler buraya gönderilecek, veriler alındıktan sonra soket kapatılacak
-		//diğer durumlarda bağlantı sağlanmayacaktır.
-		const path = reqUrl.path;
-		if(path == '/device')
-		{
-			device(connection);
-		}
-		else if (path == '/data')
-		{
-			data(connection);
-		}
-		else if (path == '/client')
-		{
-			client(connection);
-		}
-		else
-		{
-			connection.close();
-		}
-	});
+    ws.on('message', function(message) {
+        console.log('DEVICE - Received Message: ' + message.utf8Data);
+    });
 
-	function device(connection) {
-		//bir drone bağlandığında bilgisi tüm client'lere gönderilecek.
-		listDevice[id_counter] = connection;
-		console.log((new Date()) + ' Connection accepted. DEVICE-ID: ' + id_counter);
+    ws.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + ws.remoteAddress + ' disconnected.');
+    });
 
-		connection.on('message', function(message) {
-			if (message.type === 'utf8') {
-				console.log('DEVICE - Received Message: ' + message.utf8Data);
-				connection.sendUTF(message.utf8Data);
-			}
-			else if (message.type === 'binary') {
-				console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-				connection.sendBytes(message.binaryData);
-			}
-		});
-		connection.on('close', function(reasonCode, description) {
-			console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-		});		
+    for(i = 0; i<Object.keys(listClient).length; i++)
+    {
+        var key = Object.keys(listClient)[i];
+        console.log('id: '+ key);
+        var client = listClient[key];
+        var data = {'new-device':id_counter};
+        client.send(JSON.stringify(data));
+    }
+    id_counter++;
 
-		for(i = 0; i<Object.keys(listClient).length; i++)
-		{
-			var key = Object.keys(listClient)[i];
-			console.log('id: '+ key);
-			var client = listClient[key];
-			var data = {'new-device':id_counter};
-			client.sendUTF(JSON.stringify(data));
-		}
-		id_counter++;
+}
 
-	}
+function client(connection) {
+    listClient[id_counter] = connection;
+    console.log((new Date()) + ' Connection accepted. CLIENT-ID: ' + id_counter);
+    id_counter++;
 
-	function data(connection) {
+    connection.on('message', function (message) {
+        console.log(message);
+        // JSON Data = { target:'', msg:'', throttle:'', yaw:'', pitch:'', roll:'' };
+        var obj = JSON.parse(message);
+        var id = obj['id'];
+        var msg = obj['msg'];
+        var yaw = obj['yaw'];
+        var pitch = obj['pitch'];
+        var roll = obj['roll'];
+        var throttle = obj['throttle'];
 
-		// buradaki veriler client'e bağlı tüm cihazlara gönderilecek. şimdilik boş.
-		console.log((new Date()) + ' Connection accepted.');
-		connection.on('message', function(message) {
-			if (message.type === 'utf8') {
-				console.log('DATA - Received Message: ' + message.utf8Data);
-				connection.sendUTF(message.utf8Data);
-			}
-			else if (message.type === 'binary') {
-				console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-				connection.sendBytes(message.binaryData);
-			}
-		});
-		connection.on('close', function(reasonCode, description) {
-		console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-		});
-	}
+        //hedef socket(Drone) seçiliyor ve bilgiler gönderiliyor.
+        var target = listDevice[id];
+        console.log('target secildi.');
 
-
-	function client(connection) {
-		listClient[id_counter] = connection;
-		console.log((new Date()) + ' Connection accepted. CLIENT-ID: ' + id_counter);
-		id_counter++;
-
-		connection.on('message', function (message) {
-		   if(message.type === 'utf8')
-		   {
-			   console.log(message.utf8Data);
-			   // JSON Data = { target:'', msg:'', throttle:'', yaw:'', pitch:'', roll:'' };
-			   var obj = JSON.parse(message.utf8Data);
-			   var id = obj['id'];
-			   var msg = obj['msg'];
-			   var yaw = obj['yaw'];
-			   var pitch = obj['pitch'];
-			   var roll = obj['roll'];
-			   var throttle = obj['throttle'];
-
-			   console.log(JSON.stringify(obj));
-
-				//hedef socket(Drone) seçiliyor ve bilgiler gönderiliyor.	
-			   var target = listDevice[id];
-			   console.log('target secildi.');
-
-			   target.sendUTF(message.utf8Data);
-		   }
-		});
-	}
+        target.send(message);
+    });
+}
